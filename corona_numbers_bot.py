1e63c28f65662a2603555021f6ace7e363ae3bb1
Nikolai Matijevic <dev@nikolaimatijevic.de>
2020-05-21 15:28:14 +0200

    print messages of global cases

--- corona_numbers_bot.py
+++ corona_numbers_bot.py
@@ -2,7 +2,7 @@
 Get daily updates on the current numbers of Infections in your country.
 """
 
-import logging, os
+import logging, os, json, urllib.request
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler, ConversationHandler
 
@@ -11,18 +11,21 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-def start(update, context):
-    update.message.reply_text('Hi! Use /set <seconds> to set a timer')
+api_url = 'https://api.covid19api.com/summary'
 
 
-def setup(update, context):
-    """Start bot setup."""
-    update.message.reply_text('Let\'s set you up!')
-
-
-def corona_status(context):
-    job = context.job
-    context.bot.send_message(job.context, text='Beep!')
+def get(update, context):
+    with urllib.request.urlopen(api_url) as url:
+        cases = json.loads(url.read().decode())
+        global_cases = cases['Global']
+    
+        global_total_confirmed = str(global_cases['TotalConfirmed'])
+        global_total_deaths = str(global_cases['TotalDeaths'])
+        global_total_recovered = str(global_cases['TotalRecovered'])
+        
+        update.message.reply_text('Worldwide infections: ' + global_total_confirmed)
+        update.message.reply_text('Worldwide deaths: ' + global_total_deaths)
+        update.message.reply_text('Worldwide recoveries: ' + global_total_recovered)
 
 
 def error(update, context):
@@ -35,8 +38,7 @@ def main():
 
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("setup", setup))
+    dp.add_handler(CommandHandler("get", get))
 
     dp.add_error_handler(error)
 
