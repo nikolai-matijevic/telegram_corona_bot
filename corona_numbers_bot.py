d3cc32084219e4d63bd9e04d93d1283f8b402961
Nikolai Matijevic <dev@nikolaimatijevic.de>
2020-05-25 12:54:20 +0000

    change number formatting

--- corona_numbers_bot.py
+++ corona_numbers_bot.py
@@ -15,10 +15,31 @@ logger = logging.getLogger(__name__)
 
 install_cache('covid19api_cache', backend='sqlite', expire_after=600)
 
-api_url = 'https://api.covid19api.com/summary'
+api_url = 'https://api.covid19api.com/'
+api_summary = api_url + 'summary'
+
+
+def human_format(num):
+    magnitude = 0
+    while abs(num) >= 1000:
+        magnitude += 1
+        num /= 1000.0
+    return '%.2f%s' % (num, ['', 'K', 'M'][magnitude])
+
+
+def start(update, context):
+    update.message.reply_text(
+        'Last time updated (UTC)\n\n' + \
+        emojize(':earth_americas:: ', use_aliases=True) + 'Global cases\n' + \
+        emojize(':mask:: ', use_aliases=True) + 'Infected\n' + \
+        emojize(':skull:: ', use_aliases=True) + 'Deceased\n' + \
+        emojize(':smiley:: ', use_aliases=True) + 'Recovered\n\n' + \
+        'dev@nikolaimatijevic.de\n\nhttps://github.com/nikolai-matijevic/telegram_corona_bot'
+    )
+
 
 def get(update, context):
-    response = request("GET", api_url)
+    response = request("GET", api_summary)
     cases = json.loads(response.text)
     now = time.ctime(int(time.time()))
     logger.debug("Time: {0} / Used Cache: {1}".format(now, response.from_cache))
@@ -33,10 +54,10 @@ def get(update, context):
     
     update.message.reply_text(
         last_update + '\n\n' + \
-        emojize(':earth_americas:: ', use_aliases=True) + f'{global_total_confirmed:,}' + '\n' + \
-        emojize(':mask:: ', use_aliases=True) + f'{(global_total_infected):,}' + '\n' + \
-        emojize(':skull:: ', use_aliases=True) + f'{global_total_deaths:,}' + '\n' + \
-        emojize(':smiley:: ', use_aliases=True) + f'{global_total_recovered:,}')
+        emojize(':earth_americas:: ', use_aliases=True) + human_format(global_total_confirmed) + '\n' + \
+        emojize(':mask:: ', use_aliases=True) + human_format(global_total_infected) + '\n' + \
+        emojize(':skull:: ', use_aliases=True) + human_format(global_total_deaths) + '\n' + \
+        emojize(':smiley:: ', use_aliases=True) + human_format(global_total_recovered))
 
 
 def error(update, context):
@@ -45,10 +66,12 @@ def error(update, context):
 
 
 def main():
-    updater = Updater(os.environ['TOKEN'], use_context=True)
+    #updater = Updater(os.environ['TOKEN'], use_context=True)
+    updater = Updater('1257960258:AAHaKvVgZTfXPQftC3mC0PuXW9La-Q3KkvU', use_context=True)
 
     dp = updater.dispatcher
 
+    dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("get", get))
 
     dp.add_error_handler(error)
