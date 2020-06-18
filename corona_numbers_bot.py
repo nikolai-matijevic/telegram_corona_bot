d67a0ddb8f2851e38d65dc3adf93dd80e04ce493
root <root@centos7.fritz.box>
2020-06-18 16:00:04 +0200

    get numbers globally and by country

--- corona_numbers_bot.py
+++ corona_numbers_bot.py
@@ -6,6 +6,7 @@ import logging, os, json, time
 from requests import request
 from requests_cache import install_cache
 from emoji import emojize
+from emoji import EMOJI_UNICODE
 from telegram.ext import Updater, CommandHandler
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -36,6 +37,25 @@ def human_format(num):
     return '%.2f%s' % (num, ['', 'K', 'M'][magnitude])
 
 
+def request_global_numbers():
+    response = request("GET", api_summary)
+    cases = json.loads(response.text)
+    now = time.ctime(int(time.time()))
+    logger.debug("Time: {0} / Used Cache: {1}".format(now, response.from_cache))
+
+    return cases
+
+
+def display_numbers(flag, last_update, total_confirmed, total_infected, total_deaths, total_recovered):
+    return str(
+        last_update + ' (UTC)\n\n' + \
+        emojize(flag, use_aliases=True) + human_format(total_confirmed) + '\n' + \
+        emojize(':mask:: ', use_aliases=True) + human_format(total_infected) + '\n' + \
+        emojize(':skull:: ', use_aliases=True) + human_format(total_deaths) + '\n' + \
+        emojize(':smiley:: ', use_aliases=True) + human_format(total_recovered)
+    )
+
+
 def start(update, context):
     update.message.reply_text(
         'Last time updated (UTC)\n\n' + \
@@ -43,16 +63,21 @@ def start(update, context):
         emojize(':mask:: ', use_aliases=True) + 'Infected\n' + \
         emojize(':skull:: ', use_aliases=True) + 'Deceased\n' + \
         emojize(':smiley:: ', use_aliases=True) + 'Recovered\n\n' + \
+        'Get country specific numbers by using the two character country code:\n /get DE\n\n' + \
         'dev@nikolaimatijevic.de\n\nhttps://github.com/nikolai-matijevic/telegram_corona_bot'
     )
-
+        
 
 def get(update, context):
-    response = request("GET", api_summary)
-    cases = json.loads(response.text)
-    now = time.ctime(int(time.time()))
-    logger.debug("Time: {0} / Used Cache: {1}".format(now, response.from_cache))
-    print()
+    if not context.args:
+        get_global(update, context)
+    else:
+        get_country(update, context)
+
+
+def get_global(update, context):
+    cases = request_global_numbers()
+
     global_cases = cases['Global']
 
     last_update = cases['Date']
@@ -61,12 +86,40 @@ def get(update, context):
     global_total_recovered = global_cases['TotalRecovered']
     global_total_infected = global_total_confirmed - global_total_recovered - global_total_deaths
     
-    update.message.reply_text(
-        last_update + '\n\n' + \
-        emojize(':earth_americas:: ', use_aliases=True) + human_format(global_total_confirmed) + '\n' + \
-        emojize(':mask:: ', use_aliases=True) + human_format(global_total_infected) + '\n' + \
-        emojize(':skull:: ', use_aliases=True) + human_format(global_total_deaths) + '\n' + \
-        emojize(':smiley:: ', use_aliases=True) + human_format(global_total_recovered))
+    update.message.reply_text(display_numbers(':earth_americas:: ', last_update, global_total_confirmed, global_total_infected, global_total_deaths, global_total_recovered))
+
+
+def get_country(update, context):
+    cases = request_global_numbers()
+
+    iso2 = context.args[0]
+
+    country = search_country(cases, iso2)
+
+    last_update = cases['Date']
+
+    total_confirmed = country['TotalConfirmed']
+    total_deaths = country['TotalDeaths']
+    total_recovered = country['TotalRecovered']
+    total_infected = total_confirmed - total_recovered - total_deaths
+
+    flag = search_country_flag(country['Country'])
+
+    update.message.reply_text(display_numbers(flag, last_update, total_confirmed, total_infected, total_deaths, total_recovered))
+
+
+def search_country(cases, country_code):
+    countries = cases['Countries']
+
+    for country in countries:
+        if country['CountryCode'] == country_code:
+            return country
+
+
+def search_country_flag(country):
+    for key in EMOJI_UNICODE:
+        if key == (':' + country + ':'):
+            return EMOJI_UNICODE[key] + ': '
 
 
 def error(update, context):
@@ -80,7 +133,8 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("get", get))
+    dp.add_handler(CommandHandler("help", start))
+    dp.add_handler(CommandHandler("get", get, pass_args=True))
 
     dp.add_error_handler(error)
 
