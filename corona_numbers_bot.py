20cb4399d2e6843d878cc82ac33df82c8006fc63
Nikolai Matijevic <nikolaimatijevic@live.de>
2020-12-03 16:20:59 +0100

    increase loglevel for development

--- corona_numbers_bot.py
+++ corona_numbers_bot.py
@@ -42,7 +42,7 @@ fh = logging.FileHandler('bot.log')
 fh.setLevel(logging.DEBUG)
 
 ch = logging.StreamHandler()
-ch.setLevel(logging.ERROR)
+ch.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
